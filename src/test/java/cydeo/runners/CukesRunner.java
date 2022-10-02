8afb9cd1ffff8950d60758ceeac3bd2e0027c6f1
Nesibe Ozer <kosucunesibe@gmail.com>
2022-10-02 14:36:02 +0200

    day1_CucumberIntro

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "cydeo/step_definitions"
+        glue = "cydeo/step_definitions",
+        dryRun = true
+
 )
 public class CukesRunner {
 }
