de2824b5a1d849337507f81ba3cc54d06ad83a9f
Nesibe Ozer <kosucunesibe@gmail.com>
2022-10-03 10:48:25 +0200

    day2_BACKGROUND - HOOKS

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -6,11 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "",
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "cydeo/step_definitions",
         dryRun = false,
-        tags = "@Regression and not @student"
+        tags = "@wip"
 
 )
 public class CukesRunner {
