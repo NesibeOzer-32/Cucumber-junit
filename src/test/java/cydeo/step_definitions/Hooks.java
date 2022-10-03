7d272f4182f7dad9361b2eb02b25263047434e45
Nesibe Ozer <kosucunesibe@gmail.com>
2022-10-03 12:18:11 +0200

    day2_BACKGROUND - HOOKS

---
+++ src/test/java/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,30 @@
+package cydeo.step_definitions;
+
+/*
+In the class we will be able to pass pre- & post- conditions to each scenario and each step
+ */
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
+
+public class Hooks {
+    @Before
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+    @After
+    public void teardownScenario(){
+        System.out.println("====Closing browser using cucumber @ After");
+        System.out.println("====Scenario ended/ Take screenshot if failed");
+    }
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("---------> applying setup using @BeforeStep");
+    }
+    @AfterStep
+    public void afterStep(){
+        System.out.println("---------> applying teardown using @AfterStep");
+    }
+}
