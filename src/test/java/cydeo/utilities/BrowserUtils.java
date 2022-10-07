e18b6a36f7386f9449f97fcb7cea62f7dfe64ef5
Nesibe Ozer <kosucunesibe@gmail.com>
2022-10-07 18:08:43 +0200

    day4_PARAMETERIZATION_ DATATABLES REVIEW

--- src/test/java/cydeo/utilities/BrowserUtils.java
+++ src/test/java/cydeo/utilities/BrowserUtils.java
@@ -4,8 +4,11 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -15,11 +18,11 @@ public class BrowserUtils {
     This method will accept int (in seconds) and execute Thread.sleep
     for given duration
      */
-    public static void sleep(int second){
-        second *=1000;
+    public static void sleep(int second) {
+        second *= 1000;
         try {
             Thread.sleep(second);
-        }catch (InterruptedException e ) {
+        } catch (InterruptedException e) {
 
         }
     }
@@ -31,20 +34,20 @@ public class BrowserUtils {
     Arg3: expectedInTitle to be compared against actualTitle
      */
 
-    public static void switchWindowAndVerify(String expectedInUrl, String expectedInTitle){
-        Set<String> allWindowsHandles= Driver.getDriver().getWindowHandles();
+    public static void switchWindowAndVerify(String expectedInUrl, String expectedInTitle) {
+        Set<String> allWindowsHandles = Driver.getDriver().getWindowHandles();
 
-        for (String each:allWindowsHandles ){
+        for (String each : allWindowsHandles) {
             Driver.getDriver().switchTo().window(each);
 
             System.out.println("current URL: " + Driver.getDriver().getCurrentUrl());
-            if(Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+            if (Driver.getDriver().getCurrentUrl().contains(expectedInUrl)) {
                 break;
             }
         }
 
         //5. Assert:Title contains “expectedInTitle”
-        String actualTitle= Driver.getDriver().getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
     }
 
@@ -52,19 +55,32 @@ public class BrowserUtils {
     This method accepts a String "expectedTitle" and Asserts if it is true
      */
 
-    public static void verifyTitle(String expectedTitle){
+    public static void verifyTitle(String expectedTitle) {
 
-        Assert.assertEquals(Driver.getDriver().getTitle(),expectedTitle);
+        Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
     }
 
     /**
      * This method accept a String as expected value and verify actual URL CONTAINS the value
+     *
      * @param expectedInURL
      */
-    public static void verifyTitleContains(String expectedInURL){
+    public static void verifyTitleContains(String expectedInURL) {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
     }
 
+    public static List<String> dropdownOptionsAsString(WebElement dropdownElement) {
+
+        Select select = new Select(dropdownElement);
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+        return actualOptionsAsString;
 
 
+    }
 }
