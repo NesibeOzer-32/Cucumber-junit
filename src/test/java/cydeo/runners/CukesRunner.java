f76728003c970cc4ea6f49f04bbbd82b11c67055
Nesibe Ozer <kosucunesibe@gmail.com>
2022-10-07 16:04:11 +0200

    day4_PARAMETERIZATION_ DATATABLES REVIEW

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "cydeo/step_definitions",
         dryRun = false,
-        tags = "@scenarioOutline"
+        tags = "@wip"
 
 )
 public class CukesRunner {
